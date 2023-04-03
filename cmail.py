a67336d84184876b7a1e892ac29ed133c2477f4b
MakkaRajani <rajanimakka16@gmail.com>
2023-04-03 12:51:57 +0530

    'ok'

---
+++ cmail.py
@@ -0,0 +1,17 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('rajanimakka16@gmail.com ','kpymtflqnzyjzdpk')
+    msg=EmailMessage()
+    msg['From']='rajanimakka16@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+
+
+        
+    
